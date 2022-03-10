ea07a96b79306df9d6a3e1adfc29521dd039748a
Gabriel Paschoal <gpaschoal@MMB027553SPO.local>
2022-03-10 15:19:25 -0300

    created project v1

---
+++ Letreco/Components/Keyboard/KeyCell.swift
@@ -0,0 +1,56 @@
+//
+//  KeyCell.swift
+//  Letreco
+//
+//  Created by Gabriel Paschoal on 10/03/22.
+//
+
+import UIKit
+
+class KeyCell: UICollectionViewCell {
+    static let identifier = "KeyCell"
+    
+    let label: UILabel = {
+       
+        let label  = UILabel()
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor =  .white
+        label.textAlignment = .center
+        label.font = .systemFont(ofSize: 18, weight: .medium)
+        
+        return label
+        
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = .darkGray
+        contentView.addSubview(label)
+        
+        NSLayoutConstraint.activate([
+            label.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            label.trailingAnchor.constraint(equalTo:contentView.trailingAnchor),
+            label.topAnchor.constraint(equalTo: contentView.topAnchor),
+            label.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+        ])
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+        label.text = nil
+        
+    }
+    
+    func configure(with letter: Character) {
+        label.text = String(letter).uppercased()
+    }
+    
+}
---
+++ Letreco/Components/Main View/ViewController.swift
@@ -0,0 +1,143 @@
+//
+//  ViewController.swift
+//  Letreco
+//
+//  Created by Gabriel Paschoal on 09/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    let answers = [
+        "after", "later", "block", "there", "ultra"
+    ]
+    
+    var answer = ""
+    
+    private var guesses: [[Character?]] = Array(repeating: Array(repeating: nil, count: 5), count: 6)
+    
+    let boardVC = BoardViewController()
+    let keyboardVC = KeyboardViewController()
+    
+    let nameApp: UILabel = {
+       
+        let name = UILabel()
+        
+        name.text = "LETRECO DARK MODE"
+        name.textAlignment = .center
+        name.textColor = .white
+        name.font = .systemFont(ofSize: 22, weight: .bold)
+        name.translatesAutoresizingMaskIntoConstraints = false
+        
+        return name
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        answer = answers.randomElement() ?? "after"
+        
+        view.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0.2)
+        addChildren()
+    }
+
+    private func addChildren() {
+        addChild(boardVC)
+        view.addSubview(boardVC.view)
+        boardVC.didMove(toParent: self)
+        boardVC.datasource = self
+        boardVC.view.translatesAutoresizingMaskIntoConstraints = false
+        
+        addChild(keyboardVC)
+        view.addSubview(keyboardVC.view)
+        keyboardVC.didMove(toParent: self)
+        keyboardVC.delegate = self
+        keyboardVC.view.translatesAutoresizingMaskIntoConstraints = false
+
+        view.addSubview(nameApp)
+        
+        setconfig()
+    }
+    
+    func setconfig() {
+     
+        
+        NSLayoutConstraint.activate([
+        
+            boardVC.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            boardVC.view.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            boardVC.view.topAnchor.constraint(equalTo: nameApp.topAnchor, constant: 15),
+            boardVC.view.bottomAnchor.constraint(equalTo: keyboardVC.view.topAnchor),
+            boardVC.view.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.6),
+
+            keyboardVC.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            keyboardVC.view.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            keyboardVC.view.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            
+            nameApp.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            nameApp.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            nameApp.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+
+        ])
+    }
+
+}
+
+extension ViewController: KeyboardViewControllerDelegate {
+    func keyboardViewController(_ vc: KeyboardViewController, didTapKey letter: Character) {
+        
+        var stop = false
+        
+        for i in 0..<guesses.count {
+            for j in 0..<guesses[i].count {
+                if guesses[i][j] == nil {
+                    guesses[i][j] = letter
+                    stop = true
+                    break
+                }
+            }
+            
+            if stop {
+                break
+            }
+            
+        }
+        
+        boardVC.reloadData()
+        
+    }
+    
+}
+
+extension ViewController: BoardViewControllerDatasource {
+    var currentGuesses: [[Character?]] {
+        return guesses
+    }
+    
+    func boxColor(at indexPath: IndexPath) -> UIColor? {
+        
+        let rowIndex = indexPath.section
+        
+        let count = guesses[rowIndex].compactMap({ $0 }).count
+        
+        guard count == 5 else {
+            return nil
+        }
+        
+        let indexedAnswer = Array(answer)
+        
+        guard let letter = guesses[indexPath.section][indexPath.row],
+              indexedAnswer.contains(letter) else {
+            return nil
+        }
+        
+        
+        if indexedAnswer[indexPath.row] == letter {
+            return .systemGreen
+        }
+        
+        return .systemOrange
+        
+    }
+}
